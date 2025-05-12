0b9ffb3649883e69c043c8564a92b37c9197b2ff
Isaac Farias <isaacfarias006@gmail.com>
2025-05-12 14:27:49 -0300

    add menu

--- menu.gd
+++ menu.gd
@@ -1,7 +1,9 @@
 extends Control
 
+@onready var button: Button = $CenterContainer/VBoxContainer/Button
+
 func _ready():
-	$VBoxContainer/Button.pressed.connect(_on_jogar_pressed)
+	button.pressed.connect(_on_jogar_pressed)
 
 func _on_jogar_pressed():
 	get_tree().change_scene_to_file("res://src/game.tscn")
